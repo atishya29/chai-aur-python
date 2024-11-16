93cbc7822535d0d6ade66f83605dc1d11a897fa1
atishya29 <legendsfanclub17@gmail.com>
2024-11-16 18:37:15 +0000

    datatypes started

---
+++ 02_datatypes/numbers.py
@@ -0,0 +1,7 @@
+# Number datatype in python is very strong in comparision to other languages
+
+# we have basic arithmatic operations
+# + - * / % **(divide ka result kesa chie us base pr single divivde aur double divide use krlete hai)
+
+# next if u want to see about operation precedence google it(in real world it is of no use)
+# we should always perform operations on same datatypes to make the code more readiable
