478327ff40544fc4ad3cd0822e07f4d2ea97bd97
atishya29 <legendsfanclub17@gmail.com>
2024-11-23 11:28:34 +0000

    some more questions on loops completed

---
+++ 04_loops/07_solution.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input("Enter number between 1 and 10: "))
+    if (1 <= number <= 10):
+        print("Thanks")
+        break
+    else:
+        print("Enter a valid number")
