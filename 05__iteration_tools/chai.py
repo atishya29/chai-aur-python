b7a0b319e8b8cb9bba483fae72adfa1c6e1c64b2
atishya29 <legendsfanclub17@gmail.com>
2024-11-24 08:06:30 +0000

    behind the scene of loops

---
+++ 05__iteration_tools/chai.py
@@ -0,0 +1,54 @@
+import time
+print("chai is here")
+username = "hitesh"
+print(username)
+
+
+
+# f = open('chai.py')
+# f.readline() # ise bhi file read ki jaaskti h aur ye exception ko bhi ache se handle krta h i.e last m empty string return krdeta h
+
+
+# f = open('chai.py')
+# >>> f.__next__()
+# 'import time\n'
+# >>> f.__next__()
+# 'print("chai is here")\n'
+# >>> 
+# >>> f.__next__()
+# 'username = "hitesh"\n'
+# >>> f.__next__()
+# 'print(username)'
+# >>> f.__next__()
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# StopIteration
+# >>> 
+
+
+
+
+
+# >>> for line in open('chai.py'):
+# ...     print(line)
+# ... 
+# import time
+
+# print("chai is here")
+
+# username = "hitesh"
+
+# print(username)
+
+
+
+# >>> f = open('chai.py')
+# >>> while True:
+# ...     line = f.readline()
+# ...     if not line: break
+# ...     print(line, end='')
+# ... 
+# import time
+# print("chai is here")
+# username = "hitesh"
+# print(username)
