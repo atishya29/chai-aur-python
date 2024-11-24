b7a0a5175667b98bc2d059d6a11fa57aec4c3a00
atishya29 <legendsfanclub17@gmail.com>
2024-11-24 16:09:58 +0000

    basic ques on function done

---
+++ 06_functions/03_solution.py
@@ -0,0 +1,6 @@
+def multiple(a, b):
+    return a * b
+
+print(multiple(8, 5))
+print(multiple("a", 5))
+print(multiple(8, "a"))
---
+++ 06_functions/05_solution.py
@@ -0,0 +1,5 @@
+def greet(name = "user"):
+    return "Hello, " + name + " !"
+
+print(greet("Atishya"))
+print(greet())
